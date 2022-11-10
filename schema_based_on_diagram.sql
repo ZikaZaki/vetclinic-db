8d8adca05ce562df9e8f9d23fd5c5a944a9e166d
ZikaZaki <zika.github@gmail.com>
2022-11-11 01:55:51 +0300

    Craeate medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,9 +5,18 @@ DROP DATABASE vet_clinic;
 CREATE DATABASE clinic WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' TABLESPACE = pg_default CONNECTION
 LIMIT
   = -1 IS_TEMPLATE = False;
+
 -- CREATE TABLE patients;
   CREATE TABLE patients (
-    id int,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(200),
     date_of_birth date
+  );
+
+  -- CREATE TABLE medical_histories;
+  CREATE TABLE medical_histories (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    patient_id int REFERENCES patients(id),
+    status VARCHAR(20),
   );
