b859fe2ae66dfd60491df71cdb1cf7e5e40e2db7
ZikaZaki <zika.github@gmail.com>
2022-11-11 01:51:44 +0300

    Create patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,9 @@ DROP DATABASE vet_clinic;
 CREATE DATABASE clinic WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' TABLESPACE = pg_default CONNECTION
 LIMIT
   = -1 IS_TEMPLATE = False;
+-- CREATE TABLE patients;
+  CREATE TABLE patients (
+    id int,
+    name varchar(200),
+    date_of_birth date
+  );
