c5622f49ecc0364c4570bcb9553adb2ebce748bc
ZikaZaki <zika.github@gmail.com>
2022-11-11 01:50:13 +0300

    Create clinic DB

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,7 @@
+-- ======================week 2 day 2==========================
+-- DROP DATABASE IF EXISTS clinic;
+DROP DATABASE vet_clinic;
+-- CREATE DATABASE clinic;
+CREATE DATABASE clinic WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' TABLESPACE = pg_default CONNECTION
+LIMIT
+  = -1 IS_TEMPLATE = False;
