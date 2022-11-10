8a7c75fb251066160ad3547f5b04459a7bba6f80
ZikaZaki <zika.github@gmail.com>
2022-11-10 10:22:12 +0300

    Add more indexes to reduce execuation time

--- schema.sql
+++ schema.sql
@@ -103,4 +103,8 @@ ADD
   COLUMN email VARCHAR(120);
 CREATE INDEX idx_visits_animal_id ON visits(animal_id);
 CREATE INDEX idx_visits_vet_id ON visits(vet_id);
+CREATE INDEX idx_visits_date_of_visit ON visits(date_of_visit);
+CREATE INDEX idx_visits_animal_id_vet_id ON visits(animal_id, vet_id);
 CREATE INDEX idx_owners_email ON owners(email);
+CREATE INDEX idx_owners_id ON owners(id);
+CREATE INDEX idx_owners_full_name ON owners(full_name);
