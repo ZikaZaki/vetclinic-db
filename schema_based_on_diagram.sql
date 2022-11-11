5268b3a1947c679af6c6e542070d876d0f29795d
ZikaZaki <zika.github@gmail.com>
2022-11-11 17:44:27 +0300

    Update db name

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,6 +1,6 @@
 -- ======================week 2 day 2==========================
 -- DROP DATABASE IF EXISTS clinic;
-DROP DATABASE vet_clinic;
+DROP DATABASE clinic;
 -- CREATE DATABASE clinic;
 CREATE DATABASE clinic WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' TABLESPACE = pg_default CONNECTION
 LIMIT
